46309eb1e19098f799711117f4471056520027ff
Caio <caiocolli22@gmail.com>
2025-05-31 22:08:46 -0300

    feat método puro responsável por salvar e deletar uma imagem com base em parametros

--- src/controller/users/userController.php
+++ src/controller/users/userController.php
@@ -150,57 +150,23 @@ class UserController
     {
         $user = $request->getAttribute('user');
 
-        $uploadFile = $request->getUploadedFiles();
+        $file = UserModel::GetUserImage($user['User_ID']);
+        $oldImagePatch = realpath(__DIR__ . '/../../../public/uploads/' . $file['User_Image'])  ;
 
-        if (empty($uploadFile['User_Image'])) {
-            return Response::Return400($response, 'Nenhuma imagem enviada');
+        if ($file && isset($file['User_Image'])) {
+            FileHelper::DeleteUserImage($oldImagePatch);
         }
 
-        $image = $uploadFile['User_Image'];
+        $saved = FileHelper::UploadFile($request, $response, '../../../public/uploads/usersImages/', 'usersImages/');
 
-        $maxFileSize = 500 * 1024;
-
-        if ($image->getSize() > $maxFileSize) {
-            return Response::Return400($response, 'Arquivo muito grande. Tamanho máximo de 500kb.');
+        // Verifica se há response, se tiver retorna uma response do método UploadFile
+        if ($saved instanceof PsrResponse) {
+            return $saved;
         }
 
-        $fileName = $image->getClientFilename();
-        $extension = pathinfo($fileName, PATHINFO_EXTENSION);
-        $allowedExtensions = ['jpg', 'jpeg', 'png'];
+        UserModel::UserPostImage($user['User_ID'], $saved);
 
-        if (!in_array(strtolower($extension), $allowedExtensions)) {
-            return Response::Return400($response, 'Formato de imagem não aceito');
-        }
-
-        $directory = __DIR__ . '/../../../public/uploads/usersImages';
-
-        if (!is_dir($directory)) {
-            mkdir($directory, 0775, true);
-        }
-
-        $oldImagePatch = UserModel::GetUserImage($user['User_ID']);
-
-        if ($oldImagePatch && isset($oldImagePatch['User_Image'])) {
-            FileHelper::DeleteUserImage($oldImagePatch['User_Image']);
-        }
-
-        $baseName = bin2hex(random_bytes(8));
-        $fileName = sprintf('%s.%s', $baseName, $extension);
-
-        $image->moveTo($directory . '/' . $fileName);
-
-        $patchToSave = 'usersImages/' . $fileName;
-
-        $saved = UserModel::UserPostImage($user['User_ID'], $patchToSave);
-
-        if ($saved) {
-            return Response::Return200($response, 'Imagem salva com sucesso!');
-        } else {
-            unlink($directory . '/' . $fileName);
-            return Response::Return400($response, 'Erro ao salvar imagem');
-        }
-
-        return Response::Return200($response, $image);
+        return Response::Return200($response, 'Imagem salva com sucesso!');
     }
 
     public function UserDelete(PsrRequest $request, PsrResponse $response)
--- src/helpers/fileHelper.php
+++ src/helpers/fileHelper.php
@@ -2,14 +2,57 @@
 
 namespace helpers;
 
+use response\Response;
+
 class FileHelper
 {
+    public static function UploadFile($request, $response, $patch, $folder)
+    {
+        $uploadFile = $request->getUploadedFiles();
+
+        if (empty($uploadFile['User_Image'])) {
+            return Response::Return400($response, 'Nenhuma imagem enviada');
+        }
+
+        $image = $uploadFile['User_Image'];
+
+        $maxFileSize = 500 * 1024;
+
+        if ($image->getSize() > $maxFileSize) {
+            return Response::Return400($response, 'Arquivo muito grande. Tamanho máximo de 500kb.');
+        }
+
+        $fileName = $image->getClientFilename();
+        $extension = pathinfo($fileName, PATHINFO_EXTENSION);
+        $allowedExtensions = ['jpg', 'jpeg', 'png'];
+
+        if (!in_array(strtolower($extension), $allowedExtensions)) {
+            return Response::Return400($response, 'Formato de imagem não aceito');
+        }
+
+        $directory = realpath(__DIR__ . $patch);
+
+        if (!is_dir($directory)) {
+            echo 'Pasta não criada, criando uma...';
+            mkdir($directory, 0775, true);
+        }
+
+        $baseName = bin2hex(random_bytes(8));
+        $fileName = sprintf('%s.%s', $baseName, $extension);
+
+        $image->moveTo($directory . '/' . $fileName);
+
+        $patchToSave = $folder . $fileName;
+
+        return $patchToSave;
+    }
+
     public static function DeleteUserImage($patch)
     {
-        $fullPatch = __DIR__ . '../../../public/uploads/' . $patch;
+        var_dump($patch);
 
-        if ($fullPatch && file_exists($fullPatch)) {
-            unlink($fullPatch);
+        if ($patch && file_exists($patch)) {
+            unlink($patch);
         }
     }
 }
